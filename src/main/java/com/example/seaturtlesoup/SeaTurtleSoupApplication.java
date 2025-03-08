3d9bdee1715da3d7a993fe1833f1062a82fbc3ca
juan <crumbled23@gmail.com>
2025-03-09 01:25:28 +0900

    first commit

---
+++ src/main/java/com/example/seaturtlesoup/SeaTurtleSoupApplication.java
@@ -0,0 +1,13 @@
+package com.example.seaturtlesoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaTurtleSoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaTurtleSoupApplication.class, args);
+    }
+
+}
